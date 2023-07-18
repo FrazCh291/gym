8f1d615dbc7b6731c61847374cd8df7134a61219
Muhammad Aslam <muhammad.zahid@clearcustoms.io>
2023-07-18 16:06:22 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('app');
+// });
+Route::get('{any?}', function () {
     return view('app');
-});
+})->where('any', '.*');
 Route::post('/login', [UserController::class, 'login'])->name('login');
