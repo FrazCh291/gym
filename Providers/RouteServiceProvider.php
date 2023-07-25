1fd1f956a70c1535efe8fe1ab66f83c089662904
Irfan Iqbal <irfan.iqbal@alfamohuha.com>
2023-07-26 00:03:01 +0500

    .

--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Providers;
+namespace Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use function App\Providers\base_path;
 
 class RouteServiceProvider extends ServiceProvider
 {
